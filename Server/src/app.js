04f0ff1064e07120ff5b6d84acbee96e53727eda
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-06-21 23:21:23 +0530

    final build

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,27 +1,59 @@
-import express from 'express';
-import morgan from 'morgan';
-import cookieParser from 'cookie-parser';
-import apiRoutes from './routes/index.routes.js';
-import cors from 'cors'
+import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import apiRoutes from "./routes/index.routes.js";
+import cors from "cors";
+
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+const __filename = fileURLToPath(
+  import.meta.url
+);
+
+const __dirname = path.dirname(
+  __filename
+);
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin:
+      process.env.NODE_ENV ===
+      "production"
+        ? true
+        : "http://localhost:5173",
     credentials: true,
   })
 );
 
-app.use(morgan('dev')); 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', apiRoutes);
+
+app.use("/api", apiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(
+      path.join(
+        __dirname,
+        "../../Client/dist"
+      )
+    )
+  );
+
+  app.use((req, res) => {
+    res.sendFile(
+      path.join(
+        __dirname,
+        "../../Client/dist/index.html"
+      )
+    );
+  });
+}
 
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
 export default app;
